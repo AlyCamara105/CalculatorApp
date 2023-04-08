6a1cd4c2015ecb673322cb63484f7b0dcec4432f
Aly <alcamara237@gmail.com>
2023-04-07 22:41:56 -0400

    Added a getResult function.

--- Calculator.java
+++ Calculator.java
@@ -4,8 +4,8 @@ public class Calculator {
     
     public static void main(String [] args) {
         Scanner scan = new Scanner(System.in);
-        float num1, num2, result;
-        String operand, moveOn = "continue";
+        double num1, num2, result;
+        String operator, moveOn = "continue";
 
         while (moveOn.equals("continue")) {
             System.out.println("Enter the first number");
@@ -17,27 +17,31 @@ public class Calculator {
             scan.nextLine();
 
             System.out.println("Enter the operation");
-            operand = scan.nextLine();
+            operator = scan.nextLine();
 
-            if (operand.equals("*")) {
-                result = num1 * num2;
-                printResults(num1, num2, operand, result);
-            } else if (operand.equals("/")) {
-                result = num1 / num2;
-                printResults(num1, num2, operand, result);
-            } else if (operand.equals("+")) {
-                result = num1 + num2;
-                printResults(num1, num2, operand, result);
-            } else if (operand.equals("-")) {
-                result = num1 - num2;
-                printResults(num1, num2, operand, result);
-            }
+            result = getResult(num1, num2, operator);
+            printResults(num1, num2, operator, result);
+
             System.out.println("Type \"continue\" to use the calculator one more time and anything else to stop");
             moveOn = scan.nextLine();
         }
     }
 
-    static void printResults(float num1, float num2, String operand, float result) {
-        System.out.println("" + num1 + " " + operand + " " + num2 + " is: " + result);
+    static void printResults(double num1, double num2, String operator, double result) {
+        System.out.println("" + num1 + " " + operator + " " + num2 + " is: " + result);
+    }
+
+    static double getResult(double num1, double num2, String operator) {
+        if (operator.equals("*")) {
+            return num1 * num2;
+        } else if (operator.equals("/")) {
+            return num1 / num2;
+        } else if (operator.equals("+")) {
+            return num1 + num2;
+        } else if (operator.equals("-")) {
+            return num1 - num2;
+        } else {
+            return 0;
+        }
     }
 }
